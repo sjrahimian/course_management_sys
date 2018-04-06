f62932576e205a37417fa0ef6d95763765fb726c
Sama Rahimian <srahimia@uwo.ca>
2018-04-06 17:27:12 -0400

    minor changes in main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import authenticatedUsers.*;
 import authenticationServer.AuthenticationToken;
+import com.sun.deploy.util.StringUtils;
 import loggedInUserFactory.LoggedInUserFactory;
 
 import java.io.*;
 import java.util.*;
 import java.time.*;
 import java.time.format.*;
+import java.util.regex.Pattern;
 
 class CMS{
     private static Boolean sys_state = false;  //system is off
@@ -29,12 +31,15 @@ class CMS{
             System.out.print("User ID: ");
             id = input.next();
 
-            if(!db.containsUser(Integer.parseInt(id))){
+            //check for non-numeric characters
+            if(Pattern.matches("[a-zA-Z0-9]+", id)){
+                System.out.println("Invalid entry, try again.\n");
+            }
+            else if(!db.containsUser(Integer.parseInt(id))){
                 System.out.println("No such user. Try again.\n");
             }
             else{   //user id in database
-                possibleUser = db.getAllData(Integer.parseInt(id));
-                //try for password
+                possibleUser = db.getAllData(Integer.parseInt(id)); //try for password
                 System.out.print("Password: ");
                 //disable input.next() --> for IDE testing, then enable following 3 lines for console testing
                 password = input.next();
@@ -53,7 +58,7 @@ class CMS{
                             DateTimeFormatter dtf = DateTimeFormatter.ofPattern("EEE, d MMMM, YYYY HH:mm:ss z VV ");
                             ZonedDateTime now = ZonedDateTime.now();
                             String OS = System.getProperty("os.name");
-                            System.out.println("\n\tCollege of Oarsmanship: Course Management System (CMS)");
+                            System.out.println("\n\tTrump University: Course Management System (CMS)");
                             System.out.println("\tLogging in on " + OS + " on " + now.format(dtf) + "\n");
 
                             //generate whenever someone logs in and set methods
